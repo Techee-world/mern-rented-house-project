97ba5be56ec47af0a965a63cac65edf58b79f28d
muhammed navas <muhammednavas6287@gmail.com>
2023-10-21 11:25:55 +0530

    set server side

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
-const app = express();
- 
+const app = express()
+
+
+mongoose.connect(process.env.MONGOCONNECT)
+.then(()=>{
+    console.log('mongo db is connecting');
+})
+.catch((err)=>{
+    console.log(err);
+})
+
 app.listen(3000,()=>{
     console.log('server is running port 3000 !!');
 })
